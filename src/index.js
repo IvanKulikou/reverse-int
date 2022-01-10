3f4669e09a68f5ddb5952f6e035b34f5249ea4d4
Ivan Kulikou <valfson@gmail.com>
2022-01-10 22:45:05 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let newString = "";
+    let str = Math.abs(n).toString();
+    console.log(str);
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString = newString + str[i];
+    }
+    console.log(newString);
+    return newString;
+};
